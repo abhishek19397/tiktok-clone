b9f5c1643845fbb9e3e6f334fcb98854e3f73300
abhishek19397 <abhishek.tiwari19397@gmail.com>
2020-06-28 22:10:44 +0530

    Config changes

--- config/firebase.js
+++ config/firebase.js
@@ -3,14 +3,14 @@ import firebase from "firebase";
 require("firebase/firestore");
 
 const config = {
-  apiKey: "AIzaSyAJHHWMoJ_D60TYACQ6gUQ0dzq2ksumpn8",
-  authDomain: "clonetk-app.firebaseapp.com",
-  databaseURL: "https://clonetk-app.firebaseio.com",
-  projectId: "clonetk-app",
-  storageBucket: "clonetk-app.appspot.com",
-  messagingSenderId: "644029319101",
-  appId: "1:644029319101:web:e0a0113a145041f6daf4d4",
-  measurementId: "G-VS846T9V9R",
+  apiKey: "",
+  authDomain: ",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 firebase.initializeApp(config);
